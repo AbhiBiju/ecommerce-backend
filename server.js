820dd53dce6b679f0d8c9de7452b2ca687072ef6
AbhiBiju <abhinavbiju29@gmail.com>
2021-09-06 10:36:23 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const con = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
