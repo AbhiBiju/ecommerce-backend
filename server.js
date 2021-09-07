757de78180ba8eaa696db74e95446f0453c343e5
AbhiBiju <abhinavbiju29@gmail.com>
2021-09-07 10:47:09 -0400

    create model columns

--- server.js
+++ server.js
@@ -14,4 +14,5 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
